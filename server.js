be79e608d79f1c637e1c9427530e7302b0660e60
sheetal <esmearya.001@gmail.com>
2021-05-23 15:19:07 +0530

    add rectification script

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT=process.env.PORT || 3000;
+const cors=require('cors');
 
 app.use(express.static('public'));  //for css
 app.use(express.json());            //for json
 
 const connectDB = require('./config/db');
 connectDB();
-
+//Cors
+const corsOptions = {
+    origin:['http://localhost:3000','http://localhost:5000','http://localhost:3300', 'http://localhost:8080']
+}
+app.use(cors(corsOptions));
 //Template engine
 app.set('views',path.join(__dirname,'/views')); //for ejs
 app.set('view engine','ejs');
