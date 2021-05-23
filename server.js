32e0b22f18b911f24a572f5ddc1a432ff75b8756
sheetal <esmearya.001@gmail.com>
2021-05-23 15:44:08 +0530

    adding port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const connectDB = require('./config/db');
 connectDB();
 //Cors
 const corsOptions = {
-    origin:['http://localhost:3000','http://localhost:5000','http://localhost:3300', 'http://localhost:8080']
+    origin:['http://localhost:3000','http://localhost:5000','http://localhost:3300', 'http://localhost:8080', 'http://127.0.0.1:3000']
 }
 app.use(cors(corsOptions));
 //Template engine
